f7fe3ecc691b1b8a6419751cae31bd6b06931399
mpochard <mpochard@student.42.fr>
2023-07-21 14:46:06 +0200

    clean it

--- target/debug/deps/THE_MATRIX-e458c4380231d9d6.d
+++
@@ -1,8 +0,0 @@
-/Users/macbookair/42/Matrix/target/debug/deps/THE_MATRIX-e458c4380231d9d6: src/main.rs src/types/matrix.rs src/types/test_vector_simple_operations.rs src/types/vector.rs
-
-/Users/macbookair/42/Matrix/target/debug/deps/THE_MATRIX-e458c4380231d9d6.d: src/main.rs src/types/matrix.rs src/types/test_vector_simple_operations.rs src/types/vector.rs
-
-src/main.rs:
-src/types/matrix.rs:
-src/types/test_vector_simple_operations.rs:
-src/types/vector.rs:
